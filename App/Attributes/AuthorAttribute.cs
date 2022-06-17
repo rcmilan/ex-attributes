3299a17c6cc43123bcb566f3e15ec382a605dc61
Ricardo Milan <ricardo@fluencypass.com>
2022-06-17 18:59:21 -0300

    AuthorAttribute

---
+++ App/Attributes/AuthorAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Attributes
+{
+    public class AuthorAttribute : Attribute
+    {
+        private string name;
+        public double version;
+
+        public AuthorAttribute(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+    }
+}
--- App/Entities/User.cs
+++ App/Entities/User.cs
@@ -1,7 +1,9 @@
-﻿using App.Interfaces;
+﻿using App.Attributes;
+using App.Interfaces;
 
 namespace App.Entities
 {
+    [Author("Author123")]
     public class User : IEntity
     {
         public string Name { get; set; }
