b97d600a30ce79c21d24c3ed13f399aade41c6b7
Ricardo Milan <ricardo@fluencypass.com>
2022-06-17 09:31:13 -0300

    null check

--- App/Filters/DayActionFilter.cs
+++ App/Filters/DayActionFilter.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace App.Filters
 {
@@ -13,7 +13,7 @@ namespace App.Filters
         {
             var param = context.ActionArguments.SingleOrDefault(p => p.Value is int);
 
-            if((int)param.Value < 0)
+            if (param.Value == null || (int)param.Value < 0)
             {
                 context.Result = new BadRequestObjectResult("Input não pode ser menor que 0");
                 return;
